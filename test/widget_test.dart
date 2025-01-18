3b0ebd2a377cfcbafd4e26152f47b89673cd8ccc
Sagar Kumar <sagarkumar2006@outlook.com>
2025-01-18 15:18:54 +0530

    widget_test.dart update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,8 +14,8 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-    await tester.pumpAndSettle();
-    // Verify that our counter starts at 0.
+
+    // Verify that the counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
@@ -27,4 +27,5 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
 }
